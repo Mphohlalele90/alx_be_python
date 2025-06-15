de55e6aa9e2df742518488aafe4db6d75a3e6b59
Mphohlalele90 <Mphohlalele90@gmail.com>
2025-06-15 14:35:02 +0200

    fixed global variables

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,7 +1,7 @@
 # temp_conversion_tool.py
 
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR=5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR=9/5
 
 def convert_to_celsius(fahrenheit):
     # Uses global FAHRENHEIT_TO_CELSIUS_FACTOR to convert
