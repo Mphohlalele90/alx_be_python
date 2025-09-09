81386958cc66a7c7a99c5b17c441695f52f1391d
Mphohlalele90 <Mphohlalele90@gmail.com>
2025-09-09 11:34:10 +0200

    fixed - temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -5,8 +5,8 @@ Converts temperatures between Celsius and Fahrenheit using global conversion fac
 """
 
 # Global conversion factors
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
     """Convert Fahrenheit temperature to Celsius."""
